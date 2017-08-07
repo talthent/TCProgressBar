c170f9a23089bd82d2f40ed4276883d1e2c28aa8
Tal Cohen <talcohen@Tals-MacBook-Pro-3.local>
2017-08-08 00:11:06 +0300

    Example

---
+++ Example/Example/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by Tal Cohen on 07/08/2017.
+//  Copyright © 2017 Tal Cohen. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
