9790c81260fca509c8acabcbad80945d670c2098
Tal Cohen <talcohen@Tals-MacBook-Pro-3.local>
2017-08-09 00:43:24 +0300

    Example

--- TCProgressBarExample/TCProgressBarExample/ViewController.swift
+++ TCProgressBarExample/TCProgressBarExample/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 import TCProgressBar
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var progressBar: TCProgressBar!
     
-    override func viewDidAppear(_ animated: Bool) {
-        self.progressBar.value = 1
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let progressBar = TCProgressBar(frame: CGRect(x: 120,
+                                                      y: 300,
+                                                      width: 170,
+                                                      height: 30))
+        progressBar.outlineColor = .black
+        progressBar.outlineWidth = 2
+        progressBar.spacing = 4
+        progressBar.progressColor = .darkGray
+        progressBar.backgroundColor = .lightGray
+        progressBar.value = 0.7
+        
+        self.view.addSubview(progressBar)
     }
 
 }
